ef87ddc338f793eebdc9694ab70ad5fd17201c6d
arjun <arjunsreedar@yahoo.com>
2025-03-03 15:53:15 +0530

    open AI integrated

--- toolboxai/src/components/ControlPanel.jsx
+++ toolboxai/src/components/ControlPanel.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import { PdfViewer as PdfComponent } from '../subcomponents';
 
-const ControlPanel = ({ file, pageNumber, numPages, setPageNumber, scale, setScale }) => {
-    const isFirstPage = pageNumber === 1;
-    const isLastPage = pageNumber === numPages;
-    
-    const goToFirstPage = () => !isFirstPage && setPageNumber(1);
-    const goToPreviousPage = () => !isFirstPage && setPageNumber(pageNumber - 1);
-    const goToNextPage = () => !isLastPage && setPageNumber(pageNumber + 1);
-    const goToLastPage = () => !isLastPage && setPageNumber(numPages);
-    
-    const onPageChange = (e) => setPageNumber(Number(e.target.value));
-    
+const ControlPanel = ({scale, setScale }) => {
     const isMinZoom = scale < 0.6;
     const isMaxZoom = scale >= 2.0;
     
@@ -20,15 +10,6 @@ const ControlPanel = ({ file, pageNumber, numPages, setPageNumber, scale, setSca
     
     return (
       <PdfComponent.ControlPanelContainer>
-        {/* <PdfComponent.ControlGroup>
-          <PdfComponent.Icon className={`fas fa-fast-backward ${isFirstPage ? 'disabled' : 'clickable'}`} onClick={goToFirstPage} />
-          <PdfComponent.Icon className={`fas fa-backward ${isFirstPage ? 'disabled' : 'clickable'}`} onClick={goToPreviousPage} />
-          <span>
-            Page <PdfComponent.PageInput type="number" min={1} max={numPages || 1} value={pageNumber} onChange={onPageChange} /> of {numPages}
-          </span>
-          <PdfComponent.Icon className={`fas fa-forward ${isLastPage ? 'disabled' : 'clickable'}`} onClick={goToNextPage} />
-          <PdfComponent.Icon className={`fas fa-fast-forward ${isLastPage ? 'disabled' : 'clickable'}`} onClick={goToLastPage} />
-        </PdfComponent.ControlGroup> */}
         <PdfComponent.ControlGroup>
           <PdfComponent.Icon className={`fas fa-search-minus ${isMinZoom ? 'disabled' : 'clickable'}`} onClick={zoomOut} />
           <span>{(scale * 100).toFixed()}%</span>
